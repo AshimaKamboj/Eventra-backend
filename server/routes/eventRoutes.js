66ad0540aa7256ef967cb363aa7ef201ed680f92
AshimaKamboj <ashimakamboj27@gmail.com>
2025-09-10 17:29:11 +0530

    updated evenRoutes with controller

--- server/routes/eventRoutes.js
+++ server/routes/eventRoutes.js
@@ -1,12 +1,24 @@
 // routes/eventRoutes.js
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const controller = require('../controllers/eventController');
 
-router.post('/', controller.createEvent);
-router.get('/', controller.getEvents);
-router.get('/:id', controller.getEventById);
-router.put('/:id', controller.updateEvent);
-router.delete('/:id', controller.deleteEvent);
+const auth = require("../middleware/authMiddleware");
+const authorizeRole = require("../middleware/roleMiddleware");
+const {
+  createEvent,
+  getEvents,
+  getEventById,
+  updateEvent,
+  deleteEvent,
+} = require("../controllers/eventController");
+
+// Public routes
+router.get("/", getEvents);
+router.get("/:id", getEventById);
+
+// Protected routes (organizer only)
+router.post("/", auth, authorizeRole("organizer"), createEvent);
+router.put("/:id", auth, authorizeRole("organizer"), updateEvent);
+router.delete("/:id", auth, authorizeRole("organizer"), deleteEvent);
 
 module.exports = router;
