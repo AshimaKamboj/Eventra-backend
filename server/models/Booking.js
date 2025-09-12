1b9da4aa07d0129141d7f6680eecced29200455a
AshimaKamboj <ashimakamboj27@gmail.com>
2025-09-12 14:37:11 +0530

    updated booking model

--- server/models/Booking.js
+++ server/models/Booking.js
@@ -1,11 +1,28 @@
+// server/models/Booking.js
 const mongoose = require("mongoose");
 
 const bookingSchema = new mongoose.Schema({
-  user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-  event: { type: mongoose.Schema.Types.ObjectId, ref: "Event", required: true },
-  ticketType: { type: String, default: "General" },
-  qrCode: { type: String }, // URL from GoQR API
-  createdAt: { type: Date, default: Date.now }
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  event: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Event",
+    required: true,
+  },
+  ticketType: {
+    type: String,
+    required: true,
+  },
+  qrCode: {
+    type: String, // we’ll store the GoQR image URL
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
 module.exports = mongoose.model("Booking", bookingSchema);
