643b9c2db363a656740f14cfe5722f9c31dbe998
Divyangna Moudgil <divyangna111105@gmail.com>
2026-05-21 21:55:29 +0530

    fix API baseURL for production

--- client/src/config/api.js
+++ client/src/config/api.js
@@ -1,14 +1,8 @@
 import axios from "axios";
 
-// Create an Axios instance
-const API_URL = process.env.REACT_APP_API_URL;
-
-if (!API_URL) {
-  console.error('API URL is not configured. Check your .env file.');
-}
-
 const API = axios.create({
-  baseURL: API_URL,
+  baseURL: "https://eventra-backend-pr6v.onrender.com/api",
+  withCredentials: true,
 });
 
 export default API;
